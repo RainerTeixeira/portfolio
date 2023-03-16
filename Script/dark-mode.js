8c2830a53719b011b7b3bc59b1f79fab989791e3
Rainer Teixeira <raineroliveira94@hotmail.com>
2023-03-16 17:54:12 -0300

    portiflio...

--- Script/dark-mode.js
+++ Script/dark-mode.js
@@ -1,13 +1,17 @@
-/*function toggleDarkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }*/
+const chk = document.getElementById('chk');
+const isDarkMode = localStorage.getItem('darkMode');
+
+// verifica se o modo escuro foi definido anteriormente
+if (isDarkMode === 'true') {
+  document.body.classList.add('dark');
+  chk.checked = true;
+}
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+  // salva o status do modo escuro no localStorage
+  localStorage.setItem('darkMode', chk.checked);
+});
 
 
- 
- 
- const chk = document.getElementById('chk')
 
- chk.addEventListener('change', () => {
-   document.body.classList.toggle('dark-mode')
- })
