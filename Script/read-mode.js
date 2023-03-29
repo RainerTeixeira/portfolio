bc3ede719a4192f629d7b0dbbe6f80017a256fb9
RainerTeixeira <raineroliveira94@hotmail.com>
2023-03-29 00:13:06 -0300

    portfolio ....

--- Script/read-mode.js
+++ Script/read-mode.js
@@ -1,5 +1,7 @@
-<script>
-    document.querySelector('.label').onclick = function() {
-        document.body.classList.toggle('read-mode');
+function toggleReadMode() {
+    if (document.body.classList.contains('read-mode')) {
+        document.body.classList.remove('read-mode');
+    } else {
+        document.body.classList.add('read-mode');
     }
-</script>
+}
